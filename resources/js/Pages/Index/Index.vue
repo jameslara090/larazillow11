18da93db229362419b26a04f55aa8acc44d8b46c
Wiljames Lara <james.lee.lara@gmail.com>
2024-10-26 19:17:05 +0800

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Hello World! {{ counter++ }}</h1>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const counter = ref(0)
+setInterval(() => counter.value++, 1000)
+</script>
